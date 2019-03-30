17ae7905b352d7779c09111681f71ce2f7df2f7a
Deborah Seow <deborahseow9@gmail.com>
2019-03-30 10:55:30 -0500

    Updates on API from github

--- src/App.js
+++ src/App.js
@@ -3,26 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+      githubInfo: []
+    }
+
+  handleClick = e => {
+    e.preventDefault();
+    fetch('https://github.com/users/akakuro4ever/repos', {
+      credentials: 'include', // Useful for including session ID (and, IIRC, authorization headers)
+      mode: 'no-cors'
+    })
+    .then(response => response.json())
+    .then(data=> {this.setState({githubInfo: [data]})
+    });
+  }
+
+  newHandleClick = e => {
+    const req = new XMLHttpRequest()
+    req.addEventListener("load", showRepositories);
+    req.open("GET", 'https://api.github.com/users/octocat/repos')
+    req.send()
+  }
+
+  function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          <a href="#" onClick={this.newHandleClick}>
+            Click here to Find some of Deborah's Projects
           </a>
+          <p> Welcome to the Learn About Deborah Page!</p>
+          <div id="respositories">
+          {this.state.githubInfo}
+          </div>
         </header>
       </div>
     );
   }
 }
+
 
 export default App;
